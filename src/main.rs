fcca1598445f2974a607e2158e8a4d1bc6d28ab8
youkliedoe <you@example.com>
2023-04-29 18:53:32 +0200

    first hello world check

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
-use std::net::IpAddr;
+use std::collections::HashSet;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+use std::net::{IpAddr, Ipv4Addr};
 use trust_dns_resolver::config::{ResolverConfig, ResolverOpts};
 use trust_dns_resolver::TokioAsyncResolver;
+
+static ZERO_IP_ADDRESS: IpAddr = IpAddr::V4(Ipv4Addr::new(0, 0, 0, 0));
 
 async fn resolve_domain(domain: &str) -> Result<Vec<IpAddr>, String> {
     let resolver = TokioAsyncResolver::tokio(ResolverConfig::default(), ResolverOpts::default())
@@ -14,25 +19,38 @@ async fn resolve_domain(domain: &str) -> Result<Vec<IpAddr>, String> {
     Ok(response.iter().collect())
 }
 
-fn is_domain_blocked(domain: &str, blocklist: &[&str]) -> bool {
-    blocklist.contains(&domain)
+async fn lookup_ip_adress(domain: &str, hash_set: HashSet<String>) -> Result<Vec<IpAddr>, String> {
+    if hash_set.contains(domain) {
+        return Ok(vec![ZERO_IP_ADDRESS]);
+    } else {
+        return resolve_domain(domain).await;
+    }
+}
+
+fn process_file(file_path: &str) -> HashSet<String> {
+    let file = File::open(file_path).unwrap();
+    let reader = BufReader::new(file);
+    let mut hash_set = HashSet::new();
+
+    for line in reader.lines() {
+        let line = line.unwrap();
+        let parts: Vec<&str> = line.split(' ').collect();
+
+        if parts.len() == 2 {
+            let domain = parts[1].trim();
+            hash_set.insert(domain.to_string());
+        }
+    }
+
+    hash_set
 }
 
 #[tokio::main]
 async fn main() {
-    let domain = "blocked.example.com";
-    let blocklist = vec![
-        "blocked.example.com",
-        "another.example.com",
-        "third.ex.a.m.p.example.com",
-    ];
+    let domain_test = "zzmalwo.pl";
+    let file_path = "hosts/hosts.txt";
+    let blocklist_hashset = process_file(file_path);
 
-    if is_domain_blocked(domain, &blocklist) {
-        println!("The domain {} is blocked.", domain);
-    } else {
-        match resolve_domain(domain).await {
-            Ok(ips) => println!("The domain {} resolved to: {:?}", domain, ips),
-            Err(e) => eprintln!("Error: {}", e),
-        }
-    }
+    let answer = lookup_ip_adress(domain_test, blocklist_hashset).await;
+    println!("Lookup for {}: {:?}", domain_test, answer);
 }
